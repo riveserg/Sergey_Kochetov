6a263c354004f7c06921721340767f6dd9e0e20b
Sergey Kochetov <riveserg@gmail.com>
2017-03-16 23:17:25 +0300

    javadocs

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,9 +1,24 @@
 package ru.job4j;
-public class Calculate{
+
+/**
+* Class Calculate for "Hello word!"
+* @author Sergey Kochetov
+* @since 16.03.17
+* @version 1.1
+*	
+*/
+    public class Calculate{
+/**
+* Main method
+* @param args - args
+*/
+	
 
 public static void main (String[] args){
 
-	System.out.println("Hello World");
-}
+	
+	System.out.println("Hello World!");
+    }
+
 
 }
