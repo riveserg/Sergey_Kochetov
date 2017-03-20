0eb49bd38a133ee5e237e7467207d47ec4c626c0
Sergey Kochetov <riveserg@gmail.com>
2017-03-20 19:08:45 +0300

    ArrayDuplicate2

--- chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
  */
 class ArrayDuplicate {
     /**
-     *  Method sorting array of strings.
+     *  Method sorting array of strings!.
      * @param array - array
      * @return result array
      */
