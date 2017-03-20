4e24cea39de5cf41844915cc7ff8f04a82ca5f5b
Sergey Kochetov <riveserg@gmail.com>
2017-03-20 19:03:48 +0300

    ArrayFinal2

--- chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -9,41 +9,29 @@ import java.util.Arrays;
  * @version $Id$
  * @since 0.1
  */
-public class ArrayDuplicate {
+class ArrayDuplicate {
     /**
      *  Method sorting array of strings.
      * @param array - array
      * @return result array
      */
-    public String[] remove(String[] array) {
+    String[] remove(String[] array) {
         int count = 0;
+        String tmp;
+        boolean flag;
         for (int i = 0; i < array.length; i++) {
-            for (int j = 1 + i; j < array.length; j++) {
-                if (!array[i].equals("") && array[i].equals(array[j])) {
-                    array[j] = "";
+            flag = true;
+            for (int j = 1 + i; j < array.length - count; j++) {
+                if (flag && array[i].equals(array[j])) {
+                    flag = false;
+                    tmp = array[j];
+                    array[j] = array[array.length - 1 - count];
+                    array[array.length - 1 - count] = tmp;
                     count++;
+                    i--;
                 }
             }
         }
-        return Arrays.copyOf(sortArray(array), array.length - count);
-    }
-
-    /**
-     *  Own method for sorting rows.
-     * @param array - array
-     * @return sorted array
-     */
-    private String[] sortArray(String[] array) {
-        String tmpString;
-        for (int i = 0; i < array.length - 1; i++) {
-            for (int j = 0; j < array.length - i - 1; j++) {
-                if (array[i].equals("")) {
-                    tmpString = array[i + 1];
-                    array[i + 1] = array[i];
-                    array[i] = tmpString;
-                }
-            }
-        }
-        return array;
+        return Arrays.copyOf(array, array.length - count);
     }
 }
