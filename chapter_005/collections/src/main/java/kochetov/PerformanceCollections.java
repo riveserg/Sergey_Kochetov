63bd8e6f9ae41625906352a4bfe61b38c52e8db3
Sergey Kochetov <riveserg@gmail.com>
2017-03-29 21:48:14 +0300

    Collections

---
+++ chapter_005/collections/src/main/java/kochetov/PerformanceCollections.java
@@ -0,0 +1,45 @@
+package kochetov;
+
+import java.util.Collection;
+import java.util.Iterator;
+/**
+ * PerformanceCollections.
+ *
+ * @author Kochetov
+ * @version $Id$
+ * @since 0.1
+ */
+public class PerformanceCollections {
+    /**
+     * Method add amount elements.
+     * @param collection - collection
+     * @param line - data
+     * @param amount - amount
+     * @return execution time im ms
+     */
+    public long add(Collection<String> collection, String line, int amount) {
+        final long startTime = System.currentTimeMillis();
+        for (int i = 0; i < amount; i++) {
+            collection.add(line);
+        }
+        return (System.currentTimeMillis() - startTime);
+    }
+    /**
+     * Method delete amount elements.
+     * @param collection - collection
+     * @param line - data
+     * @param amount - amount
+     * @return execution time in ms
+     */
+    public long delete(Collection<String> collection, int amount) {
+        final long startTime = System.currentTimeMillis();
+        Iterator<String> it = collection.iterator();
+        for (int i = 0; i < amount; i++) {
+            if (it.hasNext()) {
+                it.next();
+                it.remove();
+            }
+        }
+        return (System.currentTimeMillis() - startTime);
+    }
+}
---
+++ chapter_005/collections/src/test/java/kochetov/PerformanceCollectionsTest.java
@@ -0,0 +1,58 @@
+package kochetov;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.TreeSet;
+
+/**
+ * PerformanceCollectionsTest.
+ *
+ * @author Kochetov
+ * @version $Id$
+ * @since 0.1
+ */
+
+/**
+ *
+ */
+public class PerformanceCollectionsTest {
+    private final int  amount = 100_000;
+    /**
+     * Test add and remove for ArrayList.
+     */
+    @Test
+    public void whenAddAndDeleteArrayList() {
+        ArrayList<String> list = new ArrayList<String>();
+        PerformanceCollections pc = new PerformanceCollections();
+        long result = pc.add(list, "test", amount);
+        System.out.println(String.format("Add to ArrayList: %d", result));
+        result = pc.delete(list, amount);
+        System.out.println(String.format("Remove from ArrayList: %d", result));
+    }
+    /**
+     * Test add and remove for LinkedList.
+     */
+    @Test
+    public void whenAddAndDeleteLinkedList() {
+        LinkedList<String> list = new LinkedList<String>();
+        PerformanceCollections pc = new PerformanceCollections();
+        long result = pc.add(list, "test", amount);
+        System.out.println(String.format("Add to LinkedList: %d", result));
+        result = pc.delete(list, amount);
+        System.out.println(String.format("Remove from LinkedList: %d", result));
+    }
+    /**
+     * Test add and remove for TreeSet.
+     */
+    @Test
+    public void whenAddAndDeleteTreeSet() {
+        TreeSet<String> set = new TreeSet<String>();
+        PerformanceCollections pc = new PerformanceCollections();
+        long result = pc.add(set, "test", amount);
+        System.out.println(String.format("Add to TreeSet: %d", result));
+        result = pc.delete(set, amount);
+        System.out.println(String.format("Remove from TreeSet: %d", result));
+    }
+}
