03d57c59020f1109a66954aa0be476a4b363276f
Sergey Kochetov <riveserg@gmail.com>
2017-03-30 01:18:52 +0300

    Collections

--- chapter_005/collections/src/main/java/kochetov/ConvertList.java
+++ chapter_005/collections/src/main/java/kochetov/ConvertList.java
@@ -35,10 +35,12 @@ public class ConvertList {
      * @return result arrays
      */
     public int[][] toArray (List<Integer> list, int rows) {
-        int[][] result = new int[rows][rows];
+        int[][] result;
         Iterator<Integer> it = list.iterator();
         if (rows > 0) {
-            for (int i = 0; i < rows; i++) {
+            int width = list.size() % rows == 0 ? list.size()/rows : list.size()/rows + 1;
+            result = new int[width][rows];
+            for (int i = 0; i < width; i++) {
                 for (int j = 0; j < rows; j++) {
                     if (it.hasNext()) {
                         result[i][j] = it.next();
