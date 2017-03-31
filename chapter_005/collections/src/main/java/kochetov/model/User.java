9d7ed8e9e04b54e68cf30fcd69ebc954b54bb043
Sergey Kochetov <riveserg@gmail.com>
2017-04-01 00:13:09 +0300

    01/04/17 Collection

---
+++ chapter_005/collections/src/main/java/kochetov/model/User.java
@@ -0,0 +1,11 @@
+package kochetov.model;
+
+/**
+ * User.
+ *
+ * @author Kochetov
+ * @version $Id$
+ * @since 0.1
+ */
+public class User {
+}
---
+++ chapter_005/collections/src/test/java/kochetov/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by Сергей on 01.04.2017.
+ */
+package kochetov;
