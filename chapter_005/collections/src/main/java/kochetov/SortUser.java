100683895af4f24ad9d752a766d85901b841e831
Sergey Kochetov <riveserg@gmail.com>
2017-04-01 11:13:50 +0300

    01/04/17 Collections SortUser

--- chapter_005/collections/src/main/java/kochetov/SortUser.java
+++ chapter_005/collections/src/main/java/kochetov/SortUser.java
@@ -1,8 +1,8 @@
 package kochetov;
 
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import kochetov.model.User;
+
+import java.util.*;
 
 /**
  * SortUser.
@@ -23,5 +23,34 @@ public class SortUser {
             result.add(user);
         }
         return result;
+    }
+
+    /**
+     * Method sorted by hashcode.
+     * @return sorted list
+     */
+    public List<kochetov.model.User> sortHash (List<kochetov.model.User> list) {
+        Comparator<kochetov.model.User> comparator = new Comparator<kochetov.model.User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.hashCode() == o2.hashCode() ? 0 : o1.hashCode() > o2.hashCode() ? 1 : -1;
+            }
+        };
+        list.sort(comparator);
+        return list;
+    }
+    /**
+     * Method sorted by length name.
+     * @return sorted list
+     */
+    public List<User> sortLength (List<User> list) {
+        Comparator<kochetov.model.User> comparator = new Comparator<kochetov.model.User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.getName().length() == o2.getName().length() ? 0 : o1.getName().length() > o2.getName().length() ? 1 : -1;
+            }
+        };
+        list.sort(comparator);
+        return list;
     }
 }
--- chapter_005/collections/src/test/java/kochetov/SortUserTest.java
+++ chapter_005/collections/src/test/java/kochetov/SortUserTest.java
@@ -1,8 +1,11 @@
 package kochetov;
 
+import kochetov.model.*;
+import kochetov.model.User;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import static org.hamcrest.core.Is.is;
@@ -26,6 +29,7 @@ public class SortUserTest {
         list.add(new kochetov.model.User("name2", 41));
         list.add(new kochetov.model.User("name3", 81));
         list.add(new kochetov.model.User("name4", 11));
+
         Set<kochetov.model.User> set = sortUser.sort(list);
 
         String str = "";
@@ -36,5 +40,49 @@ public class SortUserTest {
 
         assertThat(str, is(result));
     }
+    /**
+     * Test.
+     */
+    @Test
+    public void whenListSortedHimByHashCode() {
+        SortUser sortUser = new SortUser();
+        ArrayList<kochetov.model.User> list = new ArrayList<>();
+        list.add(new kochetov.model.User("name1", 70));
+        list.add(new kochetov.model.User("name2", 41));
+        list.add(new kochetov.model.User("name3", 81));
+        list.add(new kochetov.model.User("name4", 11));
 
+        List<kochetov.model.User> listTwo = sortUser.sortHash(list);
+
+        String str = "";
+        for (kochetov.model.User user : listTwo) {
+            str = String.format("%s %d", str, user.hashCode());
+        }
+        final String result = " -1052833280 -1052833278 -1052833246 -1052833207";
+
+        assertThat(str, is(result));
+    }
+
+    /**
+     * Test.
+     */
+    @Test
+    public void whenListSortedHimByNameLength() {
+        SortUser sortUser = new SortUser();
+        ArrayList<kochetov.model.User> list = new ArrayList<>();
+        list.add(new kochetov.model.User("name1111", 70));
+        list.add(new kochetov.model.User("name22", 41));
+        list.add(new kochetov.model.User("name333", 81));
+        list.add(new kochetov.model.User("name4", 11));
+
+        List<kochetov.model.User> listTwo = sortUser.sortLength(list);
+
+        String str = "";
+        for (kochetov.model.User user : listTwo) {
+            str = String.format("%s %d", str, user.getName().length());
+        }
+        final String result = " 5 6 7 8";
+
+        assertThat(str, is(result));
+    }
 }
