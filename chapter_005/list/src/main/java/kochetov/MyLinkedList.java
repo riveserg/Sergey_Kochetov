124fb505a35d11d7bba45abf89bbb8b207766e38
Sergey Kochetov <riveserg@gmail.com>
2017-04-08 01:33:30 +0300

    08/04/17 MyLinkedList

---
+++ chapter_005/list/src/main/java/kochetov/MyLinkedList.java
@@ -0,0 +1,157 @@
+package kochetov;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * MyLinkedList.
+ * Created by Сергей on 07.04.2017.
+ */
+public class MyLinkedList<E> implements SimpleContainer<E> {
+    /**
+     * Size MyLinkedList.
+     */
+    int size = 0;
+    /**
+     * First object reference.
+     */
+    private MyNode<E> first;
+    /**
+     * Last object reference.
+     */
+    private MyNode<E> last;
+
+    /**
+     * Method add element to list.
+     * @param e element to add.
+     */
+    @Override
+    public void add(E e) {
+       linkedLast(e);
+    }
+    /**
+     * Method add element to end list.
+     * @param e element to add.
+     */
+    private void linkedLast(E e) {
+        final MyNode<E> l = this.last;
+        final MyNode<E> newNode = new MyNode<>(l, e, null);
+        this.last = newNode;
+        if (l == null) {
+            // if add first element
+            this.first = newNode;
+        } else {
+            l.next = newNode;
+        }
+        this.size++;
+    }
+
+    /**
+     * Return element by index.
+     * @param index - index of element.
+     * @return element
+     */
+    @Override
+    public E get(int index) {
+        this.checkPositionIndex(index);
+        MyNode<E> result;
+        if (index < (size / 2)) {
+            result = this.first;
+            for (int i = 0; i < index; i++) {
+                result = result.next;
+            }
+        } else {
+            result = this.last;
+            for (int i = this.size - 1; i > index; i--) {
+                result = result.prev;
+            }
+        }
+        return result.item;
+    }
+
+    /**
+     * The index valid data?
+     * @param index - index on MyLinkedList
+     */
+    private void checkPositionIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    /**
+     * Return size MyLinkedList.
+     * @return size
+     */
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    /**
+     * MyIterator for MyLinkedList.
+     * @return iterator
+     */
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            /**
+             * pointer.
+             */
+            private int pointer = 0;
+
+            /**
+             * The movement of the pointer possible?
+             * @return result
+             */
+            @Override
+            public boolean hasNext() {
+                return this.pointer < size;
+            }
+
+            /**
+             * Return next element with MyLinkedList.
+             * @return element
+             */
+            @Override
+            public E next() {
+                try {
+                    return get(pointer++);
+                } catch (NullPointerException e) {
+                    throw new NoSuchElementException();
+                }
+            }
+        };
+    }
+
+    /**
+     * MyNode.
+     * @param <E> generic
+     */
+    private static class MyNode<E> {
+        /**
+         * Item.
+         */
+        E item;
+        /**
+         * link to next item.
+         */
+        MyNode<E> next;
+        /**
+         * link to previous item.
+         */
+        MyNode<E> prev;
+        /**
+         * Constructor
+         * @param prev - previous item
+         * @param element - element
+         * @param next - next item
+         */
+        MyNode(MyNode<E> prev, E element, MyNode<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+}
---
+++ chapter_005/list/src/test/java/kochetov/MyLinkedListTest.java
@@ -0,0 +1,57 @@
+package kochetov;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * MyLinkedListTest.
+ * Created by Сергей on 08.04.2017.
+ */
+public class MyLinkedListTest {
+
+    /**
+     * Test.
+     */
+    @Test
+    public void whenAddElementToLinkedListThenMethodGetReturnTheCorrectValue() {
+        MyLinkedList list = new MyLinkedList();
+        list.add(111);
+        list.add(222);
+        list.add(333);
+        assertThat(list.get(2), is(333));
+    }
+    /**
+     * Test.
+     */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenDontAddElementsThenMethodGetThrowException() {
+        MyLinkedList list = new MyLinkedList();
+        list.get(0);
+    }
+    /**
+     * Test.
+     */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenAddOneElementAndMethodGetCallFirstElementThenReturnThrowException() {
+        MyLinkedList list = new MyLinkedList();
+        list.add("111");
+        list.get(1);
+    }
+
+    /**
+     * Test.
+     */
+    @Test
+    public void whenIteratorNextThenResultNextElement() {
+        MyLinkedList list = new MyLinkedList();
+        list.add("111");
+        list.add("222");
+        Iterator iterator = list.iterator();
+        assertThat(iterator.next(), is("111"));
+    }
+
+}
