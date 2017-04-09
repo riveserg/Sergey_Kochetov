4669a742eab599e7448048916480bdae9b5c6c85
Sergey Kochetov <riveserg@gmail.com>
2017-04-09 14:31:44 +0300

    09/04/17 User

---
+++ chapter_005/map/src/main/java/kochetov/model/User.java
@@ -0,0 +1,60 @@
+package kochetov.model;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ * User.
+ * Created by Сергей on 09.04.2017.
+ */
+public class User {
+    /**
+     * Name.
+     */
+    private String name;
+    /**
+     * Children.
+     */
+    private int children;
+    /**
+     * Birthday.
+     */
+    private Calendar birthday;
+
+    /**
+     * Constructor.
+     * @param name - name
+     * @param children - children
+     * @param birthday - birthday
+     */
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    /**
+     * Getter.
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Getter.
+     * @return children
+     */
+    public int getChildren() {
+        return children;
+    }
+
+    /**
+     * Getter.
+     * @return birthday
+     */
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+}
---
+++ chapter_005/map/src/main/java/kochetov/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by Сергей on 09.04.2017.
+ */
+package kochetov;
